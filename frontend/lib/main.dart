009460634c2d5df36bd90b6e3c5bdbeaf7211b7e
rety <rety>
2024-08-23 00:11:14 +0300

    Я все списал с гпт. Но оно рабоатет! Вроде как даже добавляет юзера в бд. Пока что бд указана как локалхост, константы позже сделаем

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,113 +1,201 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      routes: {'/': (context) => Register()},
+      routes: {'/': (context) => const RegistrationPage()},
     ));
 
-class Register extends StatelessWidget {
-  final _formKey = GlobalKey<FormState>();
-
-  final _emailController = TextEditingController();
-  final _firstNameController = TextEditingController();
-  final _lastNameController = TextEditingController();
-  final _passwordController = TextEditingController();
+class SignInPage extends StatelessWidget { // TODO Сделать нормальный вход
+  const SignInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Чих Пых - Регистрация'),
+        title: const Text('Sign In'),
+      ),
+      body: const Center(
+        child: Text('Sign In Page'), 
+      ),
+    );
+  }
+}
+
+class RegistrationPage extends StatefulWidget {
+  const RegistrationPage({super.key});
+
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  bool _isPasswordVisible = false;
+  bool _isConfirmPasswordVisible = false;
+
+  void _registerUser() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      final url = Uri.parse('http://127.0.0.1:8000/api/v1/auth/signup');
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'email': _emailController.text,
+          'password': _passwordController.text,
+          'full_name': _nameController.text,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        _showSnackBar(
+            'Registration successful, Your id is: ${data["id"].toString()}!');
+      } else {
+        // Handle registration error
+        _showSnackBar('Oops.. Something went Wrong!');
+      }
+    }
+  }
+
+  void _showSnackBar(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      duration: const Duration(seconds: 3),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a password';
+    }
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters long';
+    }
+    if (value != _confirmPasswordController.text) {
+      return 'Passwords do not match';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registration'),
       ),
       body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Эл. почта', border: OutlineInputBorder()),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Пожалуйста, введите электронную почту";
-                    }
-                    return null;
-                  },
-                  controller: _emailController,
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
                 ),
-                SizedBox(height: 16),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Фамилия', border: OutlineInputBorder()),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Пожалуйста, введите фамилию";
-                    }
-                    return null;
-                  },
-                  controller: _lastNameController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
                 ),
-                SizedBox(height: 16),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Имя', border: OutlineInputBorder()),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Пожалуйста, введите имя";
-                    }
-                    return null;
-                  },
-                  controller: _firstNameController,
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                    decoration: InputDecoration(
-                        labelText: 'Пароль', border: OutlineInputBorder()),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Пожалуйста, введите пароль";
-                      }
-                      return null;
-                    },
-                    controller: _passwordController),
-                SizedBox(height: 24),
-                Row(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          // Валидация прошла успешно, отправляем запрос на сервер
-                          try {
-                            final response = await sendRegistrationRequest(
-                                _emailController.text,
-                                _lastNameController.text,
-                                _firstNameController.text,
-                                _passwordController.text);
-                            final token = response['token']; // TODO
-
-                            // // Сохраняем токен (например, в SharedPreferences)
-                            // saveTokenLocally(token);
-                            print(token);
-
-                            // Другие действия после успешной регистрации
-                          } catch (error) {
-                            // Обработка ошибок (например, вывод сообщения об ошибке)
-                            print('Ошибка при регистрации: $error');
-                          }
-                        }
-                      },
-                      child: Text('Зарегистрироваться'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  if (!value.contains('@')) {
+                    return 'Please enter a valid email address';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: !_isPasswordVisible,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
                     ),
-                    TextButton(onPressed: () {}, child: Text('Войти')),
-                  ],
-                )
-              ],
-            ),
-          )),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible; 
+                      });
+                    },
+                  ),
+                ),
+                validator: (value) => _validatePassword(value),
+              ),
+
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _confirmPasswordController,
+                obscureText: !_isConfirmPasswordVisible, 
+                decoration: InputDecoration(
+                  labelText: 'Confirm Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isConfirmPasswordVisible = !_isConfirmPasswordVisible; 
+                      });
+                    },
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please confirm your password';
+                  }
+                  if (value != _passwordController.text) {
+                    return 'Passwords do not match';
+                  }
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 32.0),
+              ElevatedButton(
+                onPressed: _registerUser, // async function
+                child: const Text('Register'),
+              ),
+
+              const SizedBox(height: 16.0),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SignInPage()), // Navigate to SignInPage
+                  );
+                },
+                child: const Text('Already have an account? Sign In'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
